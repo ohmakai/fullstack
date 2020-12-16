9af76441ffcd674a345674eb1813d4f89dd6e73e
Sean Carberry <seanc@zillowgroup.com>
2020-12-15 17:20:18 -0800

    Add custom headers

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Nice work, Makai!');
+  res.send('Nice work, Makai!');
+});
+
+app.get('/lebowski', (req, res) => {
+  res.set('X-lebowski', 'dudeisms');
+  res.status(418);
+  res.send("Hey man, there's a beverage here!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
